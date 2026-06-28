8777205e2eae8bac8be31e1f38eb0bcdcbb8e9f7
shimiljas <iamshimil@gmail.com>
2026-06-28 23:12:20 +0530

    feat:astro-changes

---
+++ src/data/movies.js
@@ -0,0 +1,46 @@
+export const Classic_malayalam_Movies = [
+  {
+    image: "/thumbnails/spadikam.jpg",
+    title: "Spadikam",
+    year: "1995",
+    link: "https://www.youtube.com/...",
+  },
+
+  {
+    image: "/thumbnails/kireedam.jpg",
+    title: "Kireedam",
+    year: "1989",
+    link: "https://www.youtube.com/...",
+  },
+
+  {
+    image: "/thumbnails/manichitrathazhu.jpg",
+    title: "Manichitrathazhu",
+    year: "1993",
+    link: "https://www.youtube.com/...",
+  },
+  {
+    image: "/thumbnails/devasuram.jpg",
+    title: "Devasuram",
+    year: "1993",
+    link: "https://www.youtube.com/results?search_query=Devasuram+full+movie",
+  },
+  {
+    image: "/thumbnails/thenmavin-kombath.jpg",
+    title: "Thenmavin Kombath",
+    year: "1994",
+    link: "https://www.youtube.com/results?search_query=Thenmavin+Kombath+full+movie",
+  },
+  {
+    image: "/thumbnails/commissioner.jpg",
+    title: "Commissioner",
+    year: "1994",
+    link: "https://www.youtube.com/results?search_query=Commissioner+Malayalam+full+movie",
+  },
+  {
+    image: "/thumbnails/ramji-rao-speaking.jpg",
+    title: "Ramji Rao Speaking",
+    year: "1989",
+    link: "https://www.youtube.com/results?search_query=Ramji+Rao+Speaking+full+movie",
+  },
+];
