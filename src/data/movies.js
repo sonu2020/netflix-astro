abb04d4b6d96f68b65d79bf40be4ddb285d976f2
shimiljas <iamshimil@gmail.com>
2026-06-29 12:05:26 +0530

    Initial Astro Netflix project

--- src/data/movies.js
+++ src/data/movies.js
@@ -44,3 +44,299 @@ export const Classic_malayalam_Movies = [
     link: "https://www.youtube.com/results?search_query=Ramji+Rao+Speaking+full+movie",
   },
 ];
+export const Popular_Malayalam_movies = [
+  {
+    image: "/thumbnails/narasimham.jpg",
+    title: "Narasimham",
+    year: "2000",
+    link: "https://www.youtube.com/results?search_query=Narasimham+Malayalam+full+movie",
+  },
+  {
+    image: "/thumbnails/ravanaprabhu.jpg",
+    title: "Ravanaprabhu",
+    year: "2001",
+    link: "https://www.youtube.com/results?search_query=Ravanaprabhu+Malayalam+full+movie",
+  },
+
+  {
+    image: "/thumbnails/meeshamadhavan.jpg",
+    title: "Meesha Madhavan",
+    year: "2002",
+    link: "https://www.youtube.com/results?search_query=Meesha+Madhavan+Malayalam+full+movie",
+  },
+
+  {
+    image: "/thumbnails/nandanam.jpg",
+    title: "Nandanam",
+    year: "2002",
+    link: "https://www.youtube.com/results?search_query=Nandanam+Malayalam+full+movie",
+  },
+
+  {
+    image: "/thumbnails/cid-moosa.jpg",
+    title: "CID Moosa",
+    year: "2003",
+    link: "https://www.youtube.com/results?search_query=CID+Moosa+Malayalam+full+movie",
+  },
+
+  {
+    image: "/thumbnails/balettan.jpg",
+    title: "Balettan",
+    year: "2003",
+    link: "https://www.youtube.com/results?search_query=Balettan+Malayalam+full+movie",
+  },
+
+  {
+    image: "/thumbnails/udayananu-tharam.jpg",
+    title: "Udayananu Tharam",
+    year: "2005",
+    link: "https://www.youtube.com/results?search_query=Udayananu+Tharam+Malayalam+full+movie",
+  },
+
+  {
+    image: "/thumbnails/classmates.jpg",
+    title: "Classmates",
+    year: "2006",
+    link: "https://www.youtube.com/results?search_query=Classmates+Malayalam+full+movie",
+  },
+];
+export const Top_Tamil_Movies = [
+  {
+    image: "/thumbnails/enthiran.jpg",
+    title: "Enthiran (Robot)",
+    year: "2010",
+    link: "https://www.youtube.com/results?search_query=Enthiran+full+movie+AP+International",
+  },
+
+  {
+    image: "/thumbnails/baasha.jpg",
+    title: "Baasha",
+    year: "1995",
+    link: "https://www.youtube.com/results?search_query=Baasha+full+movie+official",
+  },
+
+  {
+    image: "/thumbnails/velayudham.jpg",
+    title: "Velayudham",
+    year: "2011",
+    link: "https://www.youtube.com/results?search_query=Velayudham+full+movie+official",
+  },
+
+  {
+    image: "/thumbnails/aadhavan.jpg",
+    title: "Aadhavan",
+    year: "2009",
+    link: "https://www.youtube.com/results?search_query=Aadhavan+full+movie+official",
+  },
+
+  {
+    image: "/thumbnails/sathuranga-vettai.jpg",
+    title: "Sathuranga Vettai",
+    year: "2014",
+    link: "https://www.youtube.com/results?search_query=Sathuranga+Vettai+full+movie+official",
+  },
+
+  {
+    image: "/thumbnails/kaadhal.jpg",
+    title: "Kaadhal",
+    year: "2004",
+    link: "https://www.youtube.com/results?search_query=Kaadhal+2004+full+movie+official",
+  },
+
+  {
+    image: "/thumbnails/anbe-sivam.jpg",
+    title: "Anbe Sivam",
+    year: "2003",
+    link: "https://www.youtube.com/results?search_query=Anbe+Sivam+full+movie+official",
+  },
+
+  {
+    image: "/thumbnails/pariyerum-perumal.jpg",
+    title: "Pariyerum Perumal",
+    year: "2018",
+    link: "https://www.youtube.com/results?search_query=Pariyerum+Perumal+full+movie+official",
+  },
+];
+
+export const Top_Hindi_Movies = [
+  {
+    image: "/thumbnails/hindi2.jpg",
+    title: "Dangal",
+    year: "2015",
+    link: "https://www.youtube.com/watch?v=vEjTUDjjU6A",
+  },
+
+  {
+    image: "/thumbnails/hindi3.jpg",
+    title: "Padmavat",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=vEjTUDjjU6A",
+  },
+
+  {
+    image: "/thumbnails/hindi4.jpg",
+    title: "Uri",
+    year: "2020",
+    link: "https://www.youtube.com/watch?v=AdwGOloQcAs",
+  },
+
+  {
+    image: "/thumbnails/hindi5.jpg",
+    title: "Padmavat Tamil",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=Af3cjNPhM4o",
+  },
+
+  {
+    image: "/thumbnails/hindi6.jpg",
+    title: "Kaabil",
+    year: "2017",
+    link: "https://www.youtube.com/watch?v=bROIs4zMgjo",
+  },
+];
+
+export const Top_English_Movies = [
+  {
+    image: "/thumbnails/eng2.jpg",
+    title: "Monster",
+    year: "2021",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+
+  {
+    image: "/thumbnails/eng3.jpg",
+    title: "John Wick",
+    year: "2019",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+
+  {
+    image: "/thumbnails/eng4.jpg",
+    title: "The Conjuring 2",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+
+  {
+    image: "/thumbnails/eng5.jpg",
+    title: "Body Of Files",
+    year: "2023",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+
+  {
+    image: "/thumbnails/eng6.jpg",
+    title: "Captain America",
+    year: "2012",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+
+  {
+    image: "/thumbnails/eng7.jpg",
+    title: "Tenet",
+    year: "2024",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+];
+
+export const Latest_Tamil_Movies = [
+  {
+    image: "/thumbnails/leo.jpg",
+    title: "Leo",
+    year: "2023",
+    link: "https://www.youtube.com/watch?v=Po3jStA673E",
+  },
+
+  {
+    image: "/thumbnails/vikram.jpg",
+    title: "Vikram",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=OKBMCL-frPU",
+  },
+
+  {
+    image: "/thumbnails/jailer.jpg",
+    title: "Jailer",
+    year: "2023",
+    link: "https://www.youtube.com/watch?v=Y5BeWdODPqo",
+  },
+
+  {
+    image: "/thumbnails/master.jpg",
+    title: "Master",
+    year: "2021",
+    link: "https://www.youtube.com/watch?v=UTiXQcrLlv4",
+  },
+
+  {
+    image: "/thumbnails/ponniyin-selvan.jpg",
+    title: "Ponniyin Selvan: Part 1",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=D4qAQYlgZQs",
+  },
+
+  {
+    image: "/thumbnails/kaithi.jpg",
+    title: "Kaithi",
+    year: "2019",
+    link: "https://www.youtube.com/watch?v=3g3qHyqdn4E",
+  },
+
+  {
+    image: "/thumbnails/thunivu.jpg",
+    title: "Thunivu",
+    year: "2023",
+    link: "https://www.youtube.com/watch?v=jnBZboK17_A",
+  },
+];
+
+export const Latest_Malayalam_Movies = [
+  {
+    image: "/thumbnails/kurup.jpg",
+    title: "Kurupp",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=L13AUL0HkDk",
+  },
+
+  {
+    image: "/thumbnails/oremam.jpg",
+    title: "Premam",
+    year: "2015",
+    link: "https://www.youtube.com/watch?v=pbgvTikmIMk",
+  },
+
+  {
+    image: "/thumbnails/ajagaja.jpg",
+    title: "Ajagajantharam",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+
+  {
+    image: "/thumbnails/carbon movie.jpg",
+    title: "Carbon",
+    year: "2020",
+    link: "https://www.youtube.com/watch?v=UISTDTV-PI4",
+  },
+
+  {
+    image: "/thumbnails/underworld.png",
+    title: "Underworld",
+    year: "2023",
+    link: "https://www.youtube.com/watch?v=Af3cjNPhM4o",
+  },
+
+  {
+    image: "/thumbnails/kaduva.png",
+    title: "Kaduva",
+    year: "2022",
+    link: "https://www.youtube.com/watch?v=Af3cjNPhM4o",
+  },
+
+  {
+    image: "/thumbnails/mayanadhi.png",
+    title: "Mayanadhi",
+    year: "2019",
+    link: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+  },
+];
